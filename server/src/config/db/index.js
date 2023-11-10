cb25255d4667feec69cd5bf3067ecec0f8d6f96d
SMT310 <shinizeam@gmail.com>
2023-11-10 09:50:31 +0700

    connected local SQL server

--- server/src/config/db/index.js
+++ server/src/config/db/index.js
@@ -1,28 +1,31 @@
 const sql = require('mssql');
 
-const dbConfig = {
-    user: 'sa',
-    password: 'Shinizeam310',
-    server: 'LAPTOP-ESFJ9H96\\KYSONSERVER',
-    database: 'QLSpa',
+const config = {
+    user: 'sa', // update me
+    password: 'Shinizeam310', // update me
+    server: 'LAPTOP-ESFJ9H96', // update me
+    database: 'QLSpa', // update me
     options: {
-        trustedConnection: true,
-    },
-};
-
-const pool = new sql.ConnectionPool(dbConfig);
-
-async function connect() {
-    try {
-        await pool.connect();
-        console.log('Database connected');
-    } catch (error) {
-        console.error('Database connection failed');
+        trustServerCertificate: true,
+        encrypt: true // If you are on Microsoft Azure, you need encryption
     }
 };
 
+async function connect() {
+    // sql.connect(config).then(() => {
+    //     console.log('SQL Server Connected');
+    // }).catch(err => {
+    //     console.error('Error connecting: ' + err.message);
+    // });
+    sql.connect(config).then(pool => {
+        return pool.request().query('SELECT * FROM SIGNUP') // update 'your_table' with your actual table name
+    }).then(result => {
+        console.log(result);
+    }).catch(err => {
+        console.error('Error executing query: ' + err.message);
+    });
+
+};
 module.exports = {
     connect,
-    sql,
-    pool,
 };
