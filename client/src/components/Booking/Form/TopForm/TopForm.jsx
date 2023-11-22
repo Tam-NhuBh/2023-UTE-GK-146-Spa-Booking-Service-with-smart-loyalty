f416a415b80c7471ca5d3d665ce79d338b78cded
SMT310 <shinizeam@gmail.com>
2023-11-22 23:52:36 +0700

    fix register front end

--- client/src/components/Booking/Form/TopForm/TopForm.jsx
+++ client/src/components/Booking/Form/TopForm/TopForm.jsx
@@ -1,79 +1,142 @@
-import DateField from '../DateField/DateField'
+import TextField from "@mui/material/TextField";
 import Label from '../Label'
 import TextInput from '../TextInput'
 import RadioGroup from '@mui/material/RadioGroup'
 import FormControlLabel from '@mui/material/FormControlLabel'
 import Radio from '@mui/material/Radio'
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import Axios from 'axios'
 import './TopForm.css'
-import SubLabel from '../SubLabel'
 
 const TopForm = () => {
+  const [values, setValues] = useState({
+    fullName: '',
+    birthDate: '',
+    gender: 'female',
+    email: '',
+    address: '',
+    city: '',
+    phone: '',
+    password: ''
+  });
+  const navigate = useNavigate();
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    Axios.post('http://localhost:8000/register', values)
+      .then(res => {
+        if (res.data.Status === "Success") {
+          navigate('/login');
+        } else {
+          alert("Error");
+        }
+      })
+      .catch(err => console.log(err));
+  };
+
   return (
-    <div className="flex flex-col rounded-[10px] overflow-hidden border-[1px] border-[#F8CBD3] top-form">
-      <div className="pt-4 px-5 pb-[6px] bg-[#F5637E]">
-        <span className="text-lg text-white">1. HỒ SƠ CỦA BẠN</span>
-      </div>
-      <div className="flex flex-col gap-5 p-5 lg:p-3">
-        <div className="flex md:flex-col mx-[-10px] gap-y-5">
-          <div className="flex-1 px-[10px] gap-y-[10px] flex flex-col">
-            <Label id="fullName" text="Họ và tên" isRequired />
-            <TextInput id="fullName" placeholder="Vui lòng nhập đầy đủ họ tên" />
-          </div>
-          <div className="flex-1 px-[10px] flex gap-5 lg:flex-col md:flex-row">
-            <div className="flex flex-col gap-[10px] flex-1">
-              <Label text="Ngày sinh" isRequired />
-              <DateField />
-            </div>
-            <div className="flex flex-col gap-y-[10px] flex-1">
-              <Label text="Giới tính" isRequired />
-              <RadioGroup
-                aria-labelledby="demo-radio-buttons-group-label"
-                defaultValue="female"
-                name="radio-buttons-group"
-                className="radio-group"
-              >
-                <FormControlLabel value="male" control={<Radio />} label="Nam" />
-                <FormControlLabel value="female" control={<Radio />} label="Nữ" />
-              </RadioGroup>
-            </div>
-          </div>
+    <div className="flex flex-col items-center rounded-[10px] overflow-hidden border-[1px] border-[#F8CBD3] top-form">
+      <form onSubmit={handleSubmit}>
+        <div className="pt-4 px-5 pb-[6px]">
+          <span className="text-2xl font-bold text-red-500">Đăng Ký</span>
         </div>
-        <div className="flex md:flex-col mx-[-10px] gap-y-5">
-          <div className="flex-1 px-[10px] gap-y-[10px] flex flex-col">
-            <div className="flex items-center justify-between gap-2 lg:flex-col lg:items-start">
-              <Label id="email" text="Email" isRequired />
-              <SubLabel id="email" text="Vui lòng nhập các ký tự chữ và số" />
+        <div className="flex flex-col gap-5 p-5 lg:p-3">
+          <div className="flex md:flex-col mx-[-10px] gap-y-5">
+            <div className="flex-1 px-[10px] gap-y-[10px] flex flex-col">
+              <Label id="fullName" text="Họ và tên" isRequired />
+              <TextInput
+                id="fullName"
+                placeholder="Vui lòng nhập đầy đủ họ tên"
+                onChange={e => setValues({ ...values, fullName: e.target.value })}
+              />
             </div>
-            <TextInput id="email" placeholder="Vui lòng nhập địa chỉ email" />
+            <div className="flex-1 px-[10px] flex gap-5 lg:flex-col md:flex-row">
+              <div className="flex flex-col gap-[10px] flex-1">
+                <Label text="Ngày sinh" isRequired />
+                <TextField
+                  type="date"
+                  onChange={e => setValues({ ...values, birthDate: e.target.value })}
+                  sx={{ backgroundColor: "white" }}
+                />
+              </div>
+
+              <div className="flex flex-col gap-y-[10px] flex-1">
+                <Label text="Giới tính" isRequired />
+                <RadioGroup
+                  aria-labelledby="demo-radio-buttons-group-label"
+                  defaultValue="female"
+                  name="radio-buttons-group"
+                  className="radio-group"
+                  onChange={(e) => setValues({ ...values, gender: e.target.value })}
+                >
+                  <FormControlLabel
+                    value="male"
+                    control={<Radio />}
+                    label="Nam"
+                  />
+                  <FormControlLabel
+                    value="female"
+                    control={<Radio />}
+                    label="Nữ"
+                  />
+                </RadioGroup>
+              </div>
+            </div>
           </div>
-          <div className="flex-1 px-[10px] gap-y-[10px] flex flex-col">
-            <div className="flex items-center justify-between gap-2 lg:flex-col lg:items-start">
-              <Label id="emailConfirm" text="Email xác nhận" isRequired />
-              <SubLabel id="emailConfirm" text="Vui lòng nhập các ký tự chữ và số" />
+          <div className="flex md:flex-col mx-[-10px] gap-y-5">
+            <div className="flex-1 px-[10px] gap-y-[10px] flex flex-col">
+              <div className="flex items-center justify-between gap-2 lg:flex-col lg:items-start">
+                <Label id="email" text="Email" isRequired />
+              </div>
+              <TextInput
+                id="email"
+                placeholder="Vui lòng nhập địa chỉ email"
+                onChange={e => setValues({ ...values, email: e.target.value })}
+              />
             </div>
-            <TextInput
-              id="emailConfirm"
-              placeholder="Vui lòng xác nhận lại địa chỉ email"
+            <div className="flex-1 px-[10px] gap-y-[10px] flex flex-col">
+              <div className="flex items-center justify-between gap-2 lg:flex-col lg:items-start">
+                <Label id="address" text="Địa chỉ" isRequired />
+              </div>
+              <TextInput
+                id="address"
+                placeholder="Vui lòng nhập địa chỉ hiện tại"
+                onChange={e => setValues({ ...values, address: e.target.value })}
+              />
+            </div>
+          </div>
+
+          <div className="flex md:flex-col mx-[-10px] gap-y-5">
+            <div className="flex-1 px-[10px] gap-y-[10px] flex flex-col">
+              <Label id="city" text="Thành phố" />
+              <TextInput
+                id="city"
+                placeholder="Vui lòng nhập thành phố"
+                onChange={e => setValues({ ...values, city: e.target.value })}
+              />
+            </div>
+            <div className="flex-1 px-[10px] gap-y-[10px] flex flex-col">
+              <Label id="phone" text="Điện thoại" isRequired />
+              <TextInput
+                id="phone"
+                placeholder="Vui lòng nhập số điện thoại"
+                onChange={e => setValues({ ...values, phone: e.target.value })}
+              />
+            </div>
+          </div>
+
+          <div className="flex flex-col gap-[10px]">
+            <Label text="Mật khẩu" isRequired id="pass" />
+            <TextField
+              type="password"
+              placeholder="Vui lòng nhập mật khẩu"
+              id="pass"
+              sx={{ backgroundColor: "white" }}
+              onChange={e => setValues({ ...values, password: e.target.value })}
             />
           </div>
         </div>
-
-        <div className="flex md:flex-col mx-[-10px] gap-y-5">
-          <div className="flex-1 px-[10px] gap-y-[10px] flex flex-col">
-            <Label id="address" text="Địa chỉ" />
-            <TextInput id="address" placeholder="Vui lòng nhập địa chỉ" />
-          </div>
-          <div className="flex-1 px-[10px] gap-y-[10px] flex flex-col">
-            <Label id="phone" text="Điện thoại" isRequired />
-            <TextInput id="phone" placeholder="Vui lòng nhập số điện thoại" />
-          </div>
-        </div>
-
-        <div className="flex flex-col gap-[10px]">
-          <Label text="Thành phố" isRequired id="city" />
-          <TextInput placeholder="Vui lòng nhập thành phố" id="city" />
-        </div>
-      </div>
+      </form>
     </div>
   )
 }
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,5 +1,8 @@
 import { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import TopForm from "../components/Booking/Form/TopForm/TopForm";
 import Axios from 'axios'
 
 const Register = () => {
@@ -23,55 +26,79 @@ const Register = () => {
   };
 
   return (
-    <div className="App">
-      <div className="auth-form-container">
-        <h2 className="text-2xl font-bold">Register</h2>
-        <form className="register-form" onSubmit={handleSubmit}>
-          <label className="label" htmlFor="name">
-            Full name
-          </label>
-          <input
-            className="input"
-            type='text'
-            name="name"
-            id="name"
-            placeholder="Full Name"
-            onChange={e => setValues({ ...values, fullName: e.target.value })}
-          />
-          <label className="label" htmlFor="email">
-            Email
-          </label>
-          <input
-            className="input"
-            type="email"
-            placeholder="youremail@gmail.com"
-            id="email"
-            name="email"
-            onChange={e => setValues({ ...values, email: e.target.value })}
-          />
-          <label className="label" htmlFor="password">
-            Password
-          </label>
-          <input
-            className="input"
-            type="password"
-            placeholder="********"
-            id="password"
-            name="password"
-            onChange={e => setValues({ ...values, password: e.target.value })}
-          />
-          <button className="button" type="submit">
-            Register
+    // <div className="App">
+    //   <div className="auth-form-container">
+    //     <h2 className="text-2xl font-bold">Register</h2>
+    //     <form className="register-form" onSubmit={handleSubmit}>
+    //       <label className="label" htmlFor="name">
+    //         Full name
+    //       </label>
+    //       <input
+    //         className="input"
+    //         type='text'
+    //         name="name"
+    //         id="name"
+    //         placeholder="Full Name"
+    //         onChange={e => setValues({ ...values, fullName: e.target.value })}
+    //       />
+    //       <label className="label" htmlFor="email">
+    //         Email
+    //       </label>
+    //       <input
+    //         className="input"
+    //         type="email"
+    //         placeholder="youremail@gmail.com"
+    //         id="email"
+    //         name="email"
+    //         onChange={e => setValues({ ...values, email: e.target.value })}
+    //       />
+    //       <label className="label" htmlFor="password">
+    //         Password
+    //       </label>
+    //       <input
+    //         className="input"
+    //         type="password"
+    //         placeholder="********"
+    //         id="password"
+    //         name="password"
+    //         onChange={e => setValues({ ...values, password: e.target.value })}
+    //       />
+    //       <button className="button" type="submit">
+    //         Register
+    //       </button>
+    //     </form>
+    //     <div style={{ display: 'flex', alignItems: 'center' }}>
+    //       Already have an account?&nbsp;
+    //       <Link to="/login">
+    //         <button className="link-btn button">Login here</button>
+    //       </Link>
+    //     </div>
+    //   </div>
+    // </div>
+    <form onSubmit={handleSubmit}>
+      <main className="flex items-center justify-center min-h-[100vh] bg-[#ffe6e6]">
+        <div className="w-full max-w-[1100px] flex flex-col items-center gap-7">
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <TopForm />
+          </LocalizationProvider>
+          <button className="w-[70%] max-w-[400px] bg-[#F5637E] text-white text-xl font-bold py-3 text-center rounded-lg shadow-sm">
+            ĐĂNG KÝ
           </button>
-        </form>
-        <div style={{ display: 'flex', alignItems: 'center' }}>
-          Already have an account?&nbsp;
-          <Link to="/login">
-            <button className="link-btn button">Login here</button>
-          </Link>
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              flexDirection: "column",
+            }}
+          >
+            <span>Bạn đã có tài khoản?</span>
+            <Link to="/login">
+              <button className="link-btn button">Đăng nhập ở đây</button>
+            </Link>
+          </div>
         </div>
-      </div>
-    </div>
+      </main>
+    </form>
   )
 }
 
