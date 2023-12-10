d790cbfb64a25e84752eac2f7a0d62cacd590018
TamNhuBh <tnhubh@gmail.com>
2023-12-10 10:03:33 +0700

    .gitignore is now working

--- client/src/uitls/helper/changeCurrency.js
+++
@@ -1,6 +0,0 @@
-export const formatCurrency = (amount, locale, currency) => {
-  return amount.toLocaleString(locale, {
-    style: "currency",
-    currency: currency,
-  });
-};
--- server/src/controllers/logoutController.js
+++
@@ -1,8 +0,0 @@
-class LogoutController {
-    logoutExecute(req, res, next) {
-        res.clearCookie('token');
-        return res.json({ Status: "Success" });
-    }
-}
-
-module.exports = new LogoutController;
